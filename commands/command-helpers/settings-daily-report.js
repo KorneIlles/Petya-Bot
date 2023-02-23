ca81b5b94e8f16b0543ca317a11eaf72633a940f
csipkek98 <csipkekrisztian@gmail.com>
2023-02-23 16:28:16 +0100

    Add: js file to manage daily-report subcommandGroup in settings.js

---
+++ commands/command-helpers/settings-daily-report.js
@@ -0,0 +1,44 @@
+const reportManager = require('../../actions/daily-report-manager.js')
+
+async function dailyReportFunctionManager(interaction){
+    if(interaction.options.getSubcommand() === "working"){
+        if(interaction.options.getString("value") == "on"){
+            reportManager.start()
+            await interaction.reply({
+                content:`Daily report functions started!`,
+                ephemeral: true
+            })
+        }else{
+            reportManager.stop()
+            await interaction.reply({
+                content:`Daily report functions stopped!`,
+                ephemeral: true
+            })
+        }
+    }else if(interaction.options.getSubcommand() === "closing"){
+        if(interaction.options.getString("value") == "on"){
+            reportManager.startClosing()
+            await interaction.reply({
+                content:`Daily report closing function started!`,
+                ephemeral: true
+            })
+        }else{
+            reportManager.stopClosing()
+            await interaction.reply({
+                content:`Daily report closing function stopped!`,
+                ephemeral: true
+            })
+        }
+    }else if(interaction.options.getSubcommand() === "status"){
+        const statuses = await reportManager.getStatus()
+        const open = statuses["open"]? "on" : "off"
+        const close = statuses["close"]? "on" : "off"
+        await interaction.reply({
+            content:`Current statuses:\nOpen: ${open}\nClose: ${close}`,
+            ephemeral: true
+        })
+    }
+}
+module.exports ={
+    manager: dailyReportFunctionManager
+}
