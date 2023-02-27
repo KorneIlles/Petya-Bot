84fb1f6585fbc3cd9cea6ba63f78ac16d1afe8c8
Illes Kornel <kornelilles10@egmail.com>
2023-02-27 16:51:20 +0100

    add to can handle modal interadction to the interaction-create.js

--- events/interaction-create.js
+++ events/interaction-create.js
@@ -1,23 +1,35 @@
-const { Events } = require('discord.js');
-
+const {Events, InteractionType, Client} = require('discord.js');
+const techStackModal = require('../modals/add-tech-stack-modal.js');
+const logRoomId = process.env.LOG_ROOM_ID;
 module.exports = {
     name: Events.InteractionCreate,
     async execute(interaction) {
-        if (!interaction.isChatInputCommand()) return;
+        if (interaction.isChatInputCommand()) {
 
-        const command = interaction.client.commands.get(interaction.commandName);
+            const command = interaction.client.commands.get(interaction.commandName);
 
-        if (!command) {
-            console.error(`No command matching ${interaction.commandName} was found.`);
-            return;
+            if (!command) {
+                console.error(`No command matching ${interaction.commandName} was found.`);
+                return;
+            }
+
+            try {
+                await command.execute(interaction);
+            } catch (error) {
+                console.error(`Error executing ${interaction.commandName}`);
+                console.error(error);
+                await interaction.reply({
+                    content: 'There was an error while executing this command!',
+                    ephemeral: true
+                });
+            }
+        }else if (interaction.type === InteractionType.ModalSubmit){
+           if (interaction.customId === 'techStackModal'){
+               techStackModal.execute(interaction);
+               interaction.member.client.channels.cache.get(logRoomId).send({content: interaction.fields.getTextInputValue("techStackInput").includes(',')?
+                       `Log > Added these technologies: ***${interaction.fields.getTextInputValue("techStackInput")}*** to: <@${interaction.member.user.id}>!`
+                       :`Log > Added this technology: ***${interaction.fields.getTextInputValue("techStackInput")}*** to: <@${interaction.member.user.id}>!`})
+           }
         }
-
-        try {
-            await command.execute(interaction);
-        } catch (error) {
-            console.error(`Error executing ${interaction.commandName}`);
-            console.error(error);
-            await interaction.reply({content: 'There was an error while executing this command!', ephemeral: false});
-        }
-    },
+    }
 };
