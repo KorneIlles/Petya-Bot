d2397273f207497cba428aea350c3859f98837ba
Illes Kornel <kornelilles10@egmail.com>
2023-02-27 16:54:34 +0100

    refactor utils

--- utils/utils.js
+++ utils/utils.js
@@ -1,14 +1,7 @@
-function capitalizeFirstLetter(string) {
+function capitalizeTheString(string) {
     return string.charAt(0).toUpperCase() + string.slice(1);
 }
 
-async function asyncForEach(array, callback) {
-    for (let index = 0; index < array.length; index++) {
-        await callback(array[index], index, array);
-    }
-}
-
 module.exports = {
-    capitalizeFirstLetter,
-    asyncForEach
+   capitalizeTheString
 }
